eb88c8211d53502164dde4ce42c5be727070021c
kaustubhrane12 <90202667+kaustubhrane12@users.noreply.github.com>
2022-04-05 11:46:56 +0530

    Add files via upload

---
+++ Count_of_Prime_Numbers.py
@@ -0,0 +1,24 @@
+import time
+
+t1 = time.time()
+
+
+def prime_num(n):
+    for i in range(n + 1):
+        if i > 1:
+            for j in range(2, i):
+                if i % j == 0:
+                    break
+            else:
+                yield i
+
+
+c = 0
+for i in prime_num(100000):
+    c += 1
+
+print(c)
+
+t2 = time.time() - t1
+
+print(t2)
